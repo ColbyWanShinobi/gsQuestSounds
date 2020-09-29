6f7bcb94168607a5ebbddd95afa70786fffdae26
Colby Butler <colby@owljet.com>
2020-09-29 17:08:59 -0400

    Updated for shadowlands

--- gsQuestSounds.lua
+++ gsQuestSounds.lua
@@ -6,9 +6,11 @@ web: gameshaman.com
 repo: https://github.com/ColbyWanShinobi/gsQuestSounds.git
 --]]
 
+local gsQuestSounds = CreateFrame("Frame");
+local events = {};
 local sounds = {
   --questComplete is played whenever the quest is finally completed and ready to be turned in
-  --"Sound/creature/Peon/PeonBuildingComplete1.ogg"
+  --"Sound/Creature/Peon/PeonBuildingComplete1.ogg"
   questComplete = 558132,
   
   --ObjectiveComplete is played whenever a quest objective is removed from the quest log.
@@ -16,87 +18,104 @@ local sounds = {
   objectiveComplete = 558137,
   
   --ObjectiveProgress is played whenever an accumulation quest is incremented. ie. You've found 3/5 acorns
-  --"Sound/creature/Peon/PeonWhat3.ogg"
-  objectiveProgress = 558143
-  
+  --"Sound/Creature/Peon/PeonWhat3.ogg"
+  objectiveProgress = 558143  
 };
 
 --Classic
 if MainMenuBarLeftEndCap then
   --print("Found Classic UI...");
   sounds = {
-    questComplete = "Sound/creature/Peon/PeonBuildingComplete1.ogg",
+    questComplete = "Sound/Creature/Peon/PeonBuildingComplete1.ogg",
     objectiveComplete = "Sound/Creature/Peon/PeonReady1.ogg",
-    objectiveProgress = "Sound/creature/Peon/PeonWhat3.ogg"
+    objectiveProgress = "Sound/Creature/Peon/PeonWhat3.ogg"
   };
 end
 
-local gsQuestSounds = CreateFrame("Frame");
-local events = {};
+gsQuestSounds.currentQuestId = 0;
+gsQuestSounds.currentQuestTitle = '';
+gsQuestSounds.currentQuestLevel = 0;
+gsQuestSounds.currentCompleteQuestObjectives = 0;
+gsQuestSounds.currentQuestProgressCounter = 0;
+gsQuestSounds.currentQuestProgressTable = {};
 
-gsQuestSounds.questIndex = 0;
-gsQuestSounds.questId = 0;
-gsQuestSounds.completeCount = 0;
-
-local function countCompleteObjectives(index)
-  local n = 0;
-  for i = 1, GetNumQuestLeaderBoards(index) do
-    local _, _, finished = GetQuestLogLeaderBoard(i, index);
-    if finished then
-      n = n + 1;
+local function getCompleteObjectiveCount(objectives)
+  local completeObjectives = 0;
+  for objIndex, objInfo in ipairs(objectives) do
+    --print(objIndex,objInfo.type,objInfo.text,objInfo.numRequired,objInfo.numFulfilled, objInfo.finished);
+    if (objInfo.finished) then
+      completeObjectives = completeObjectives + 1;
     end
   end
-  return n;
+  return completeObjectives;
 end
 
-function gsQuestSounds:setQuest(index)
-  self.questIndex = index;
-  if index > 0 then
-    --local _, _, _, _, _, _, _, _, id = GetQuestLogTitle(index)
-    local q = {GetQuestLogTitle(index)};
-    local id = q[8];
-    self.questId = id;
-    if id and id > 0 then
-      self.completeCount = countCompleteObjectives(index);
+local function getQuestProgressCount(objectives)
+  local updateCount = 0;
+  for objIndex, objInfo in ipairs(objectives) do
+    --print(objIndex,objInfo.type,objInfo.text,objInfo.numRequired,objInfo.numFulfilled, objInfo.finished);
+    updateCount = updateCount + objInfo.numFulfilled;
+  end
+  return updateCount;
+end
+
+local function getUpdatedQuestObjectiveString(id, oldObjectives, freshObjectives)
+  local questText = "";
+  for objIndex, objInfo in ipairs(oldObjectives) do
+    if (objInfo.numFulfilled ~= freshObjectives[objIndex].numFulfilled) then
+      questText = freshObjectives[objIndex].text;
     end
+  end
+  return questText;
+end
+
+function gsQuestSounds:setCurrentQuest(id)
+  if id and id > 0 then
+    self.currentQuestId = id;
+    local index = C_QuestLog.GetLogIndexForQuestID(id);
+    local info = C_QuestLog.GetInfo(index);
+    local level = info.level;
+    self.currentQuestLevel = level;
+    local title = info.title;
+    self.currentQuestTitle = title;
+    local objectives = C_QuestLog.GetQuestObjectives(id);
+    self.currentQuestProgressTable = objectives;
+    self.currentCompleteQuestObjectives = getCompleteObjectiveCount(objectives);
+    self.currentQuestProgressCounter = getQuestProgressCount(objectives);
   end
 end
 
-function gsQuestSounds:checkQuest()
-  if self.questIndex > 0 then
-    local index = self.questIndex;
-    self.questIndex = 0;
-    --local title, level, _, _, _, _, complete, daily, id = GetQuestLogTitle(index);
-    local q = {GetQuestLogTitle(index)};
-    local title = q[1];
-    local level = q[2];
-    local complete = q[6];
-    local daily = q[7];
-    local id = q[8];
-    local link = "";
-    if MainMenuBarLeftEndCap then
-      --print("Found Classic UI...");
-      link = title;
-    else
-      link = GetQuestLink(id);
-    end
-    if link == nil or link == "" then
-      link = "Dynamic Quest";
-    end
-    if id == self.questId then
-      if id and id > 0 then
-        local objectivesComplete = countCompleteObjectives(index);
-        if complete then
-          print("gsQuestSounds: ["..level.."] '"..link.."': complete");
-          gsQuestSounds:Play(sounds.questComplete);
-        elseif objectivesComplete>self.completeCount then
-          print("gsQuestSounds: ["..level.."] '"..link.."': objective complete ("..objectivesComplete.."/"..GetNumQuestLeaderBoards(index)..")");
-          gsQuestSounds:Play(sounds.objectiveComplete);
-        else
-          print("gsQuestSounds: ["..level.."] '"..link.."': updated");
-          gsQuestSounds:Play(sounds.objectiveProgress);
-        end
+function gsQuestSounds:checkCurrentQuest()
+  local id = gsQuestSounds.currentQuestId;
+  if id and id > 0 then
+    local title = gsQuestSounds.currentQuestTitle;
+    local level = self.currentQuestLevel;
+    local title = self.currentQuestTitle;
+    local complete = C_QuestLog.ReadyForTurnIn(id);
+    local totalObjectives = C_QuestLog.GetNumQuestObjectives(id);
+    local objectives = C_QuestLog.GetQuestObjectives(id);
+    local completeObjectives = getCompleteObjectiveCount(objectives);
+    local questProgress = getQuestProgressCount(objectives);
+    local updatedText = getUpdatedQuestObjectiveString(id, self.currentQuestProgressTable, objectives);
+    
+    if complete then
+      --Quest is complete
+      print("gsQS: ["..level.."] '"..title.."': "..updatedText);
+      print("gsQS: ["..level.."] '"..title.."': Quest Complete");
+      gsQuestSounds:Play(sounds.questComplete);
+    elseif completeObjectives > self.currentCompleteQuestObjectives then
+      --An objective is complete
+      print("gsQS: ["..level.."] '"..title.."': "..updatedText);
+      print("gsQS: ["..level.."] '"..title.."': Objective Complete ("..completeObjectives.."/"..totalObjectives..")");
+      gsQuestSounds:Play(sounds.objectiveComplete);
+    elseif questProgress > self.currentQuestProgressCounter then
+      --Quest progress is made
+      if (updatedText) then 
+        print("gsQS: ["..level.."] '"..title.."': "..updatedText);
+      else
+        print("gsQS: ["..level.."] '"..title.."': Updated");
       end
+      gsQuestSounds:Play(sounds.objectiveProgress);
     end
   end
 end
@@ -112,29 +131,28 @@ function gsQuestSounds:init()
   for k,v in pairs(events) do
     self:RegisterEvent(k);
   end
-    print("gsQuestSounds by gameshaman.com - Addon Loaded");
+    print("gsQuestSounds [gsQS] by gameshaman.com - Addon Loaded");
 end
 
 function gsQuestSounds:Play(sound)
-   --print("Playing:", sound)
   if sound and sound~="" then
     PlaySoundFile(sound);
   end
 end
 
 function events:UNIT_QUEST_LOG_CHANGED(unit)
+  -- This event triggers whenevr the quest log is updated.
   if unit=="player" then
-    gsQuestSounds:checkQuest();
+    gsQuestSounds:checkCurrentQuest();
   end
 end
 
-function events:QUEST_WATCH_UPDATE(index)
+function events:QUEST_WATCH_UPDATE(questId)
   -- This event triggers just *before* the changes are registered
   -- in the quest log, giving a great opportunity to quantify changes
   -- in the subsequent UNIT_QUEST_LOG_CHANGED
-  gsQuestSounds:setQuest(index);
+  gsQuestSounds:setCurrentQuest(questId);
 end
-
 
 -- ................................................................
 -- must be last line:
